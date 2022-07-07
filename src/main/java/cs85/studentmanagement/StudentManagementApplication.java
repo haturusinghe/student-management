12323692881e71f21031404e9813f4c1cbf9e683
S.Haturusinghe <haturusingh3@gmail.com>
2022-07-07 10:38:19 +0530

    Initial commit

---
+++ src/main/java/cs85/studentmanagement/StudentManagementApplication.java
@@ -0,0 +1,13 @@
+package cs85.studentmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagementApplication.class, args);
+	}
+
+}
