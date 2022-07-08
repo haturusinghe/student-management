4a2dfb88d3b33cd92be77dbc001c4521ce14bf46
S.Haturusinghe <haturusingh3@gmail.com>
2022-07-08 09:03:59 +0530

    added query methods

--- src/main/java/cs85/studentmanagement/student/StudentController.java
+++ src/main/java/cs85/studentmanagement/student/StudentController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "api/student")
@@ -26,14 +27,41 @@ public class StudentController {
 
     }
 
+    //Find Student By Name
+    @GetMapping(params = "name")
+    public List<Student> getStudent(@RequestParam(required = true) String name){
+        return studentService.getStudent(name);
+    }
+
+    //Find Student By Name
+    @GetMapping(params = "student_number")
+    public ResponseEntity<Student> findStudentByStudentNumber(@RequestParam(required = true) String student_number){
+        Optional<Student> result = studentService.findStudentByStudentNumber(student_number);
+
+        if(result.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(result.get());
+    }
+
     //Add New Student
     @PostMapping
     public void addStudent(@Valid @RequestBody Student student){
         studentService.addNewStudent(student);
     }
 
-    /*@GetMapping(value = "/{stu_num}")
-    public ResponseEntity<Student> findStudentByStudentNumber(@PathVariable("stu_num") String stu_num){
-        Student student = studentService.getStudent(stu_num);
+
+    /*
+
+     //Delete an Existing Student
+    @DeleteMapping(path = "{studentId}")
+    public void deleteStudent(@PathVariable("studentId") Long id ){
+        studentService.deleteStudent(id);
+
+    @PutMapping(path = "{studentId}")
+    public void updateStudent(@PathVariable("studentId") Long id,@RequestParam(required = false) String name,@RequestParam(required = false) String email){
+        studentService.updateStudent(id,name,email);
+
     }*/
 }
