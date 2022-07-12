bb59b7c7273039743494257e64a8c8852eadbb7b
S.Haturusinghe <haturusingh3@gmail.com>
2022-07-12 13:04:00 +0530

    minor

---
+++ README.md
@@ -0,0 +1,12 @@
+# Submission for CSCI 22082 - Assesment - Task 1
+## Endpoints
+
+* api/student - For Rest Controller
+* manage/student-list - For Thymleaf MVC
+
+## TODO
+
+* Add Pagination
+* Spring Security Features - Login, Register, Secure Endpoints
+* Error Handling
+
--- src/main/java/cs85/studentmanagement/StudentManagementApplication.java
+++ src/main/java/cs85/studentmanagement/StudentManagementApplication.java
@@ -3,6 +3,8 @@ package cs85.studentmanagement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+// Student Number - CS/2018/015
 @SpringBootApplication
 public class StudentManagementApplication {
 
